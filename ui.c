eb31510c983abcc25ce0879d4551b852856d2ba4
jibril04Afaa <128831088+jibril04Afaa@users.noreply.github.com>
2024-04-23 23:12:31 -0400

    basic GUI window

--- ui.c
+++ ui.c
@@ -5,6 +5,23 @@
 
 int main(int argc, char **argv) {
     gtk_init(&argc, &argv);
+
+    GtkWidget *window; // initialize window variable
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_widget_show_all(window);
+
+    // set a default size
+    gtk_window_set_default_size(GTK_WINDOW(window), 900, 900);
+
+    // set the default position to be centered
+    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+
+    // creates a callback to close the window
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    // render the window
+    gtk_widget_show_all(window);
+
     gtk_main();
     return 0;
 } 
